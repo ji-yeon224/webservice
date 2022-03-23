5b8c112f2c6871edbbf2fd021d25222313497a07
Kim-jiyeon224 <shjh0107@gmail.com>
2022-03-23 17:23:55 +0900

    테스트코드 작성

---
+++ src/main/java/com/jykim/webservice/web/HelloController.java
@@ -0,0 +1,23 @@
+package com.jykim.webservice.web;
+
+import com.jykim.webservice.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+
+}
